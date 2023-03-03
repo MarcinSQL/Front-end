c3a760dc021d0ca0e6919c511d3aa20bd9a8fb1f
Marcin <narwas@vp.pl>
2023-03-03 16:41:58 +0100

    3th commit

--- Testowe/AmatorSklep/main.js
+++ Testowe/AmatorSklep/main.js
@@ -10,11 +10,30 @@
 <p class="product-item-sale-info">Promocja</p>
 </div> */
 
-const currentProducts = products;
+let currentProducts = products;
 let categories = new Set(); //set nie pozwala na powtarzanie elementow
-
+let basket = [];
+let addToBasketButtons;
 const productSection = document.querySelector(".products");
+
+const addToBasket = (e) => {
+    const selectedId = parseInt(e.target.dataset.id);
+
+    const key = currentProducts.findIndex((product) => product.id === selectedId);   //znajduje index produktu
+
+    basket.push(currentProducts.at(key)); //dodaje caly produkt do koszyka
+    
+    const basketTotal = basket.reduce((sum, product) => {
+        return (sum += product.price - (product.saleAmont ? product.saleAmont : 0));
+      }, 0);
+
+    basketTotal > 0 ? basketClearBtn.classList.add("active") : basketClearBtn.classList.remove("active");
+
+    basketAmountSpan.innerHTML = `${basketTotal} zł`;
+};
+
 const renderProducts = (items) =>{
+    productSection.innerHTML = "";
     for(let i = 0; i < items.length; i++){
         const newProduct = document.createElement("div");
         newProduct.className = `product-item ${items[i].sale ? "on-sale" : ""}`;
@@ -26,13 +45,18 @@ const renderProducts = (items) =>{
         <span class="price">${items[i].price.toFixed(2)} zł</span>
         <span class="price-sale">${(items[i].price - items[i].saleAmount).toFixed(2)}</span>
     </div>
-    <button class="product-add-to-basket-btn">Dodaj do koszyka</button>
+    <button data-id="${items[i].id}" class="product-add-to-basket-btn">Dodaj do koszyka</button>
     <p class="product-item-sale-info">Promocja</p>
     `
 
     productSection.appendChild(newProduct);
     }
+    addToBasketButtons = document.querySelectorAll('.product-add-to-basket-btn');
+
+    addToBasketButtons.forEach((btn) => btn.addEventListener('click', addToBasket));
 };
+
+
 
 const renderCategories = (items) =>{
     for(let i = 0; i < items.length; i++){
@@ -49,6 +73,7 @@ const renderCategories = (items) =>{
     categories.forEach((category, index) =>{
         const newCategory = document.createElement('button');
         newCategory.innerHTML = category;
+        newCategory.dataset.category = category;
 
         index === 0 ? newCategory.classList.add("active") : "";
 
@@ -58,3 +83,51 @@ const renderCategories = (items) =>{
 
 document.onload = renderProducts(currentProducts); //ladowanie przy odpaleniu strony
 document.onload = renderCategories(currentProducts);
+
+const categoriesButton = document.querySelectorAll(".categories-items button");
+
+categoriesButton.forEach(btn => btn.addEventListener('click', (e) =>{
+        const category = e.target.dataset.category;
+
+        categoriesButton.forEach(btn => btn.classList.remove("active"));
+
+        e.target.classList.add("active");
+
+        currentProducts = products;
+
+        if(category === 'wszystkie') currentProducts = products;
+        else{
+            currentProducts = currentProducts.filter((product) => product.category === category)
+        }
+        
+        renderProducts(currentProducts);
+    })
+);
+
+const searchBarInput = document.querySelector('.search-bar-input');
+
+
+searchBarInput.addEventListener('input', (e) => {
+    const search = e.target.value;
+
+    const foundProducts = currentProducts.filter((product) => {
+        if(product.name.toLowerCase().includes(search.toLowerCase())) return product;
+    })
+
+    const emptyState = document.querySelector('.empty-state');
+
+    foundProducts.length === 0 ? emptyState.classList.add("active") : emptyState.classList.remove("active");
+
+    renderProducts(foundProducts);
+})
+
+const basketClearBtn = document.querySelector('.basket-clear-btn');
+const basketAmountSpan = document.querySelector(".basket-amount");
+
+const clearBasket = () => {
+    basketAmountSpan.innerHTML = "Koszyk";
+    basket = [];
+}
+
+basketClearBtn.addEventListener('click', clearBasket);
+
